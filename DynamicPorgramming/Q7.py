741e100f7632cb95cc3b881972c8a275e7950bbb
samriddha kc <samriddhakc@samriddhas-MacBook-Pro.local>
2022-01-10 10:47:42 -0600

    pascal traingle II

--- DynamicPorgramming/Q7.py
+++ DynamicPorgramming/Q7.py
@@ -12,5 +12,33 @@ class Solution:
         :return:
         '''
 
+        if rowIndex == 0:
+            return [1]
+
+        if rowIndex == 1:
+            return [1, 1]
+
+        prev = [1, 1]
+
+        # 1.....row_index sum = O(n(n+1)/2)= O(n^2) time | O(n) space.
+
+        for i in range(2, rowIndex + 1):
+            ans = [1]
+            for r_idx in range(i - 1):
+                ans.append(prev[r_idx] + prev[r_idx + 1])
+            ans.append(1)
+            prev = ans
+        return prev
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    assert ( sol.getRow(2) == [1, 2, 1] )
+    assert ( sol.getRow(3) == [1, 3, 3, 1] )
+
+
+
+
+
 
 
