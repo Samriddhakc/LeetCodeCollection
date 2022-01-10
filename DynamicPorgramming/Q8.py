e9b4539b74e856fcb436e95d30490d22d0ae45a6
samriddha kc <samriddhakc@samriddhas-MacBook-Pro.local>
2022-01-10 10:56:37 -0600

    max profit running indx problem

--- DynamicPorgramming/Q8.py
+++ DynamicPorgramming/Q8.py
@@ -6,7 +6,25 @@ Return the maximum profit you can achieve from this transaction. If you cannot a
 
 class Solution:
     def maxProfit(self, prices):
+
+        # Big O. O(len(prices)) time | O(1) space 
         '''
         :param prices:
         :return:
         '''
+
+        max_profit = 0
+        low_idx = 0
+        for i in range(1, len(prices)):
+            if (prices[i] < prices[low_idx]):
+                low_idx = i
+            else:
+                max_profit = max(max_profit, prices[i] - prices[low_idx])
+
+        return max_profit
+    #         max_prof = 0
+    #         for i in range(len(prices)):
+    #             for j in range(i +1, len(prices)):
+    #                 if prices[j] > prices[i]:
+    #                     max_prof = max(max_prof, prices[j] - prices[i])
+    #         return max_prof
